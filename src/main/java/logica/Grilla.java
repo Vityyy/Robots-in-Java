5b0d79a42c1ff2f128460d24f7c268b3cbd0f8ce
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-04-09 17:14:31 -0300

    Correciones varias de clase Grilla
    Co-Authored-By: ColinaAndres <130932179+ColinaAndres@users.noreply.github.com>

--- src/main/java/logica/Grilla.java
+++ src/main/java/logica/Grilla.java
@@ -5,28 +5,30 @@ import java.util.ArrayList;
 public class Grilla<T>{
     private final int n_filas;
     private final int n_columnas;
+    private int[] posicion_jugador;
+    private int[] posicion_robots;
+    private ArrayList<ArrayList<T>> matriz;
 
-    //private ArrayList<Pair<int, int>> robots;
 
-
-    public Grilla(int n_filas, int n_columnas) {
+    public Grilla(int n_filas, int n_columnas, int[] posicion_robots) {
         this.n_filas = n_filas;
         this.n_columnas = n_columnas;
+        this.posicion_jugador = new int[]{n_filas/2 , n_columnas/2};
+        this.posicion_robots = posicion_robots;
         crearGrilla();
     }
 
     private void crearGrilla(){
-        var arr = new ArrayList<ArrayList<T>>();
+        this.matriz = new ArrayList<ArrayList<T>>();
 
         for (int i = 0; i < n_filas; i++) {
-            var fila = new ArrayList<T>();
+            ArrayList<T> fila = new ArrayList<T>();
             for (int j = 0; j < n_columnas; j++) {
                 fila.add(null);
             }
+            matriz.add(fila);
         }
     }
-
-
-
-
+    public void otorgarPosicion(Robot robot){
+    }
 }
