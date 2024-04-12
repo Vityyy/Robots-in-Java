828edd4fb41b93bc255da894781f59d4603962c2
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-04-12 15:49:12 -0300

    Diversos cambios de la clase 'Robot'
    Ahora contiene dos clases: 'RobotSimple' y 'RobotComplejo' que implmentan la interfaz 'Enemigo'
    
    Co-Authored-By: ColinaAndres <130932179+ColinaAndres@users.noreply.github.com>

---
+++ src/main/java/logica/RobotComplejo.java
@@ -0,0 +1,21 @@
+package logica;
+
+public class RobotComplejo implements Enemigo{
+    private boolean funcional = true;
+    public void moverse(Grilla grilla) {
+        if (!this.funcional) {
+            return;
+        }
+        posicion_actual = grilla.getPosicionRobot(this);
+        coordenadas_jugador = grilla.getPosicionJugador();
+        var nueva_posicion = new int[]{posicion_actual[0], posicion_actual[1]};
+
+        for (int i = 0; i < 2; i++) {
+            nueva_posicion[0] = Integer.compare(coordenadas_jugador[0], posicion_actual[0]);
+            nueva_posicion[1] = Integer.compare(coordenadas_jugador[1], posicion_actual[1]);
+        }
+    }
+    public void setNoFuncional(){
+        this.funcional = false;
+    }
+}
---
+++ src/main/java/logica/RobotSimple.java
@@ -0,0 +1,19 @@
+package logica;
+
+public class RobotSimple implements Enemigo{
+    private boolean funcional = true;
+    public void moverse(Grilla grilla) {
+        if (!this.funcional) {
+            return;
+        }
+        posicion_actual = grilla.getPosicionRobot(this);
+        coordenadas_jugador = grilla.getPosicionJugador();
+        var nueva_posicion = new int[]{posicion_actual[0], posicion_actual[1]};
+        nueva_posicion[0] = Integer.compare(coordenadas_jugador[0], posicion_actual[0]);
+        nueva_posicion[1] = Integer.compare(coordenadas_jugador[1], posicion_actual[1]);
+    }
+    public void setNoFuncional(){
+        this.funcional = false;
+    }
+}
+
