1fb961bd82348171a05840b6480ae74549c64642
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-04-12 15:03:26 -0300

    Actualización de la clase 'Sistema'
    Creación del método 'jugarTurno' y eliminaciones varias

--- src/main/java/logica/Sistema.java
+++ src/main/java/logica/Sistema.java
@@ -14,46 +14,45 @@ public class Sistema {
     private ArrayList<Enemigo> enemigos;
     private int nivel;
     private int Score;
-    private int Tps_seguros;
+    private int tpsSeguros;
     private Grilla grilla;
 
     public Sistema() {
         this.Score = 0;
-        this.Tps_seguros = 2;
+        this.tpsSeguros = 2;
         this.nivel = 1;
         this.jugador = Jugador();
-        crear_enemigos(nivel, n_fil, n_col);
+        crearEnemigos(nivel, n_fil, n_col);
         this.grilla = Grilla(n_fil, n_col, this.jugador, this.enemigos);
     }
 
-    private void crear_enemigos(int nivel, int n_fil, int n_col) {
+    private void crearEnemigos(int nivel, int n_fil, int n_col) {
         var enemigos = new ArrayList<Enemigo>();
-        int max_enemigos = (n_fil * n_col * nivel) / 40;
-        int cant_simples = (max_enemigos * 3) / 4;
-        int cant_complejos = (max_enemigos) / 4;
+        int maxEnemigos = (n_fil * n_col * nivel) / 40;
+        int cantSimples = (maxEnemigos * 3) / 4;
+        int cantComplejos = (maxEnemigos) / 4;
 
-        for(int i = 0; i <= cant_simples; i++) {
+        for(int i = 0; i <= cantSimples; i++) {
             var enemigo = new RobotSimple();
             enemigos.add(enemigo);
         }
-        for(int i=0; i<= cant_complejos;i++) {
+        for(int i=0; i<= cantComplejos;i++) {
             var enemigo = new RobotComplejo();
             enemigos.add(enemigo);
         }
         this.enemigos = enemigos;
     }
 
-
-    public int getN_fil() {
-        return n_fil;
-    }
-    public int getN_col() {
-        return n_col;
+    public void jugarTurno(int[] coordenadas){
+        this.jugador.moverse(coordenadas,this.grilla);
+        for (Enemigo enemigo : this.enemigos){
+            enemigo.moverse(this.grilla);
+        }
     }
     public int getScore() {
         return Score;
     }
-    public int getTps_seguros() {
+    public int getTpsSeguros() {
         return Tps_seguros;
     }
 }
